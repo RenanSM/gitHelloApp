7d2541538cd2de49d7a7b20f5191e0ddbf86224f
RenanSM <renan.psm2018@gmail.com>
2022-03-14 14:10:49 -0300

    Adicionando GitHelloApp com a main() - v1.0

---
+++ src/main/java/GitHelloApp.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author devsys-b
+ */
+public class GitHelloApp {
+    public static void main(String[] args) {
+        
+    }
+    
+}
